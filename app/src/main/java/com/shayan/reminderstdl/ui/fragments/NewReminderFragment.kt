4942914156811b5fdbabb3e38f79bd8e06f31bdd
shayann07 <shayankhan17353@gmail.com>
2024-11-27 08:02:54 +0500

    .

--- app/src/main/java/com/shayan/reminderstdl/ui/fragments/NewReminderFragment.kt
+++ app/src/main/java/com/shayan/reminderstdl/ui/fragments/NewReminderFragment.kt
@@ -1,60 +1,198 @@
 package com.shayan.reminderstdl.ui.fragments
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.shayan.reminderstdl.R
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.shayan.reminderstdl.databinding.FragmentNewReminderBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NewReminderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NewReminderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentNewReminderBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val locationPermissionRequestCode = 1001
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNewReminderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        // Listen to location switch toggle
+        binding.locationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                handleLocationSwitch()
+            } else {
+                binding.currentLocationIcon.visibility = View.GONE
+            }
+        }
+
+        // Handle click on current location icon
+        binding.currentLocationIcon.setOnClickListener {
+            openLocationInGoogleMaps()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_reminder, container, false)
+    private fun handleLocationSwitch() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            getCurrentLocation()
+        } else {
+            // Request location permissions
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionRequestCode
+            )
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewReminderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewReminderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun getCurrentLocation() {
+        try {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                if (!isLocationEnabled()) {
+                    Toast.makeText(requireContext(), "Enable location services", Toast.LENGTH_SHORT)
+                        .show()
+                    return
+                }
+
+                fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                    if (location != null) {
+                        binding.currentLocationIcon.visibility = View.VISIBLE
+                    } else {
+                        // Request new location if no cached location available
+                        requestNewLocationData()
+                    }
+                }.addOnFailureListener {
+                    Toast.makeText(
+                        requireContext(), "Failed to retrieve location", Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else {
+                // Request permissions
+                requestPermissions(
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionRequestCode
+                )
+            }
+        } catch (e: SecurityException) {
+            Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun openLocationInGoogleMaps() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    val uri = "geo:${location.latitude},${location.longitude}"
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri)).apply {
+                        setPackage("com.google.android.apps.maps")
+                    }
+                    if (intent.resolveActivity(requireActivity().packageManager) != null) {
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(
+                            requireContext(), "Google Maps is not installed", Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Location not available", Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
+        } else {
+            Toast.makeText(
+                requireContext(), "Location permissions are not granted", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionRequestCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            } else {
+                Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun requestNewLocationData() {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000)
+            .setWaitForAccurateLocation(true).build()
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return // Permissions not granted, exit
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                val location = locationResult.lastLocation
+                if (location != null) {
+                    // Successfully retrieved location
+                    binding.currentLocationIcon.visibility = View.VISIBLE
+                    fusedLocationClient.removeLocationUpdates(this)
+                } else {
+                    Toast.makeText(
+                        requireContext(), "Unable to fetch new location data", Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }, Looper.getMainLooper())
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
